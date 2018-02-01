bf609777740220a5c12b062d3c95a1b6e67e53d4
Victor Petukhov <i@victor.am>
2018-02-01 23:03:27 +0300

    Added script for copying anomaly source code files in specified folder

---
+++ anomalistic_files_extractor.sh
@@ -0,0 +1,33 @@
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case ${key} in
+        -i|--anomalies_file)
+            anomalies_file="$2"
+            shift
+            shift
+            ;;
+        -o|--anomalies_folder)
+            anomalies_folder="$2"
+            shift
+            shift
+            ;;
+    esac
+done
+
+mkdir -p ${anomalies_folder}
+
+code_folder="./code"
+file_counter=1
+
+jq -r '.[] | "\(.[0]):\(.[1])"' ${anomalies_file} | while read item
+do
+    IFS=':' read -r -a element <<< "$item"
+
+    filename="${element[0]%.*}"
+    deviation="${element[1]%.*}"
+
+    cp ${code_folder}/${filename} ${anomalies_folder}/${deviation}_${file_counter}.kt
+
+    let "file_counter++"
+done
