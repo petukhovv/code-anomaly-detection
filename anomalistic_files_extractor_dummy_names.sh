c04c505c4feca263d8d255643a1433fcdcf21b72
Victor Petukhov <i@victor.am>
2018-05-01 12:53:52 +0300

    Add alternative anomalistic files extraction

--- anomalistic_files_extractor_dummy_names.sh
+++ anomalistic_files_extractor_dummy_names.sh
@@ -24,23 +24,15 @@ done
 mkdir -p ${anomalies_folder}
 
 file_counter=1
-source_dir="repos_factorized_sparsed"
 
 jq -r '.[] | "\(.[0]):\(.[1])"' ${anomalies_file} | while read item
 do
     IFS=':' read -r -a element <<< "$item"
 
-    filepath=${element[0]%.*}
-    filepath=${filepath/$source_dir/}
-    filepath=${filepath/\/cst\//\/sources\/}
-    deviation=${element[1]}
-    filename=$(basename $filepath)
-    filename=${filename%.*}
-    deviation=$(printf '%.*f\n' 3 $deviation)
+    filename="${element[0]%.*}"
+    deviation="${element[1]%.*}"
 
-    echo ${code_folder}${filepath}
-
-    cp ${code_folder}${filepath} ${anomalies_folder}/${deviation}_${filename}_${file_counter}.kt
+    cp ${code_folder}/${filename} ${anomalies_folder}/${deviation}_${file_counter}.kt
 
     let "file_counter++"
 done
